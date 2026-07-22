8f12c386ab877353fa0139e1376d5a0fb3ff1954
Shreyansh Kumar <shreyan.kumar29@gmail.com>
2026-07-22 13:25:29 +0530

    Implemented

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,15 @@
+import os
 import psycopg2
 
 
 def get_db_connection():
 
     conn = psycopg2.connect(
-        host="localhost",
-        database="attendance_db",
-        user="postgres",
-        password="Yassu16@",
-        port="5432"
+        host=os.getenv("DB_HOST", "localhost"),
+        database=os.getenv("DB_NAME", "attendance_db"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", "Yassu16@"),
+        port=os.getenv("DB_PORT", "5432")
     )
 
     return conn
